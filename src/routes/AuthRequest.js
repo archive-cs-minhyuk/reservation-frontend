cf8fcc09d800174ffdbd1ae8572418ce2bf0b969
nikestussy <smh0706@kaist.ac.kr>
2021-01-11 19:31:40 +0900

    브라우저 라우터 수정 전

--- src/routes/AuthRequest.js
+++ src/routes/AuthRequest.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 const AuthRequest = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [pwdCheck, setPwdCheck] = useState("");
   const [error, setError] = useState("");
   const onChange = (event) => {
     const {
@@ -13,10 +14,16 @@ const AuthRequest = () => {
       setEmail(value);
     } else if (name === "password") {
       setPassword(value);
+    } else if (name === "passwordCheck") {
+      setPwdCheck(value);
     }
   };
   const onSubmit = async (event) => {
     event.preventDefault();
+    if (password !== pwdCheck) {
+      alert("비밀번호가 일치하지 않습니다.");
+      return;
+    }
     try {
       await authService.createUserWithEmailAndPassword(email, password);
     } catch (error) {
@@ -40,6 +47,13 @@ const AuthRequest = () => {
           placeholder="비밀번호를 입력하세요"
           required
           value={password}
+          onChange={onChange}
+        />
+        <input
+          name="passwordCheck"
+          type="password"
+          placeholder="비밀번호를 한 번 더 입력하세요"
+          value={pwdCheck}
           onChange={onChange}
         />
         <input type="submit" value="계정 생성하기" />
